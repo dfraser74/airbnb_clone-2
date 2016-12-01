cd23dace3811f0f79ad71d9e44b5131979fe7926
Kuan <pui_kuan93@hotmail.com>
2016-12-01 15:13:50 +0800

    changed production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,20 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  config.active_job.queue_adapter = :sidekiq
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+     :address => "smtp.gmail.com",
+     :port => 587,
+     :user_name => ENV['GMAIL_USERNAME'],
+     :password => ENV['GMAIL_PASS'],
+     :authentication => :plain,
+     :enable_starttls_auto => true
+}  
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
