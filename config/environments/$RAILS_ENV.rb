0e04d487edf88b55b8f1bcfb4e13ee30d8f3b441
Kuan <pui_kuan93@hotmail.com>
2016-12-01 15:10:13 +0800

    changed production settings

--- config/environments/$RAILS_ENV.rb
+++ config/environments/$RAILS_ENV.rb
@@ -2,7 +2,7 @@ config.action_mailer.delivery_method = :smtp
 
 config.action_mailer.smtp_settings = {
      :address => "smtp.gmail.com",
-     :port => 587,
+     :port => 25,
      :user_name => ENV['GMAIL_USERNAME'],
      :password => ENV['GMAIL_PASS'],
      :authentication => :plain,
