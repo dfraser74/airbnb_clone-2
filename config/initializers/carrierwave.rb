171a56d4e34125723d31d3e22c60ce18a3cc9dfd
Kuan <pui_kuan93@hotmail.com>
2016-12-01 13:53:36 +0800

    fixing carrierwave issue

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,14 +6,14 @@ CarrierWave.configure do |config|
   # Use local storage if in development or test
   if Rails.env.development? || Rails.env.test?
     CarrierWave.configure do |config|
-      config.storage = :file
+      config.fog_provider = 'fog/aws'
     end
   end
 
   # Use AWS storage if in production
   if Rails.env.production?
     CarrierWave.configure do |config|
-      config.storage = :fog
+      config.fog_provider = 'fog/aws'
     end
   end
 
